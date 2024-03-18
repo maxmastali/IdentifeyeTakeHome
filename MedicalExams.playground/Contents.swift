44d223655c21bb840436ecc9a4365a23c09d5664
Maxwell Mastali <maxmastali@gmail.com>
2024-03-17 20:20:03 -0400

    added some code structure for records and base function

--- MedicalExams.playground/Contents.swift
+++ MedicalExams.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 import Foundation
 
+class Record {
+    let id: String
+    let name: String
+    let examCount: Int
+    
+    init(id: String, name: String) {
+        self.id = id
+        self.name = name
+        self.examCount = 0
+    }
+}
+
+func processRequests(_ requestFile: String) -> [Record] {
+    return []
+}
