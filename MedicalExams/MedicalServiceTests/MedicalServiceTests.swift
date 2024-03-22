2acf3267a2ef6b6640f503c15e94847a0812b28f
Maxwell Mastali <maxmastali@gmail.com>
2024-03-22 13:22:56 -0400

    reorganized test files

--- MedicalExams/MedicalServiceTests/MedicalServiceTests.swift
+++ MedicalExams/MedicalServiceTests/MedicalServiceTests.swift
@@ -6,7 +6,7 @@
 //
 
 import XCTest
-@testable import MedicalService
+@testable import MedicalExams
 
 // Running these tests led to a compile error that I was unable to resolve, but these are what my tests would look like :)
 class MedicalServiceTests: XCTestCase {
@@ -17,11 +17,11 @@ class MedicalServiceTests: XCTestCase {
         let patients = service.processRequests(inputGivenFile)
         
         XCTAssertEqual(patients["123"]?.name, "JOHN DOE")
-        XCTAssertEqual(patients["123"]?.id, 123)
+        XCTAssertEqual(patients["123"]?.id, "123")
         XCTAssertEqual(patients["123"]?.examCount, 0)
         
         XCTAssertEqual(patients["789"]?.name, "JANE CROW")
-        XCTAssertEqual(patients["789"]?.id, 789)
+        XCTAssertEqual(patients["789"]?.id, "789")
         XCTAssertEqual(patients["789"]?.examCount, 2)
     }
     
@@ -31,12 +31,11 @@ class MedicalServiceTests: XCTestCase {
         let patients = service.processRequests(inputGivenFile)
         
         XCTAssertEqual(patients["123"]?.name, "MAX MASTALI")
-        XCTAssertEqual(patients["123"]?.id, 543)
+        XCTAssertEqual(patients["123"]?.id, "543")
         XCTAssertEqual(patients["123"]?.examCount, 1)
         
         XCTAssertEqual(patients["789"]?.name, "RUSSELL WESTBROOK")
-        XCTAssertEqual(patients["789"]?.id, 234)
+        XCTAssertEqual(patients["789"]?.id, "234")
         XCTAssertEqual(patients["789"]?.examCount, 0)
     }
-
 }
