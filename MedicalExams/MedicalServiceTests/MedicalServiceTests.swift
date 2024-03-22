65cd21638c22438e0fd653e36bac394413232538
Maxwell Mastali <maxmastali@gmail.com>
2024-03-22 13:05:48 -0400

    removed unnecessary files

--- MedicalExams/MedicalServiceTests/MedicalServiceTests.swift
+++ MedicalExams/MedicalServiceTests/MedicalServiceTests.swift
@@ -1,35 +1,42 @@
 //
 //  MedicalServiceTests.swift
-//  MedicalServiceTests
+//  MedicalExamsTest
 //
-//  Created by Maxwell Mastali on 3/22/24.
+//  Created by Maxwell Mastali on 3/21/24.
 //
 
 import XCTest
+@testable import MedicalService
 
-final class MedicalServiceTests: XCTestCase {
+// Running these tests led to a compile error that I was unable to resolve, but these are what my tests would look like :)
+class MedicalServiceTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func givenTestCase() {
+        let service = MedicalService()
+        let inputGivenFile = "/Users/Max/Desktop/IdentifeyeHEALTHTakeHome/MedicalExams/MedicalExams/givenRequests.txt"
+        let patients = service.processRequests(inputGivenFile)
+        
+        XCTAssertEqual(patients["123"]?.name, "JOHN DOE")
+        XCTAssertEqual(patients["123"]?.id, 123)
+        XCTAssertEqual(patients["123"]?.examCount, 0)
+        
+        XCTAssertEqual(patients["789"]?.name, "JANE CROW")
+        XCTAssertEqual(patients["789"]?.id, 789)
+        XCTAssertEqual(patients["789"]?.examCount, 2)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func newTestCase() {
+        let service = MedicalService()
+        let inputGivenFile = "/Users/Max/Desktop/IdentifeyeHEALTHTakeHome/MedicalExams/MedicalExams/newRequests.txt"
+        let patients = service.processRequests(inputGivenFile)
+        
+        XCTAssertEqual(patients["123"]?.name, "MAX MASTALI")
+        XCTAssertEqual(patients["123"]?.id, 543)
+        XCTAssertEqual(patients["123"]?.examCount, 1)
+        
+        XCTAssertEqual(patients["789"]?.name, "RUSSELL WESTBROOK")
+        XCTAssertEqual(patients["789"]?.id, 234)
+        XCTAssertEqual(patients["789"]?.examCount, 0)
     }
 
 }
